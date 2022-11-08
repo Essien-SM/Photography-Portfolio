dd6cedb35587dd40fa49b4b7c8eb70109416b17f
Essien-SM <stephenyessien@gmail.com>
2022-11-08 15:29:16 +0000

    gallery section

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
 // change nav style on scroll
-window.addEventListener('scroll', () =>{
-    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
-})
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scrolled", window.scrollY > 0);
+});
 
 // CONTACT BTN (circular text btn)
 
-const textButtons = document.querySelectorAll('.contact__btn');
+const textButtons = document.querySelectorAll(".contact__btn");
 
-textButtons.forEach(textButtons => {
-    let text = textButtons.querySelector('p');
+textButtons.forEach((textButtons) => {
+  let text = textButtons.querySelector("p");
 
-    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
-})
+  text.innerHTML = text.innerHTML
+    .split("")
+    .map(
+      (character, index) =>
+        `<span style="transform: rotate(${index * 12}deg)">${character}</span>`
+    )
+    .join("");
+});
+
+// SWIPER JS (gallery section)
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    599: {
+        slidesPerView: 2,
+        spaceBetween: 40
+    },
+
+    1023: {
+        slidesPerView: 3,
+        spaceBetween: 60
+    }
+  }
+});
